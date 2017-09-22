690697cdcb8c0df9c65186e924d77ff1a4ca404e
ssw32 <ssw32@uclive.ac.nz>
2017-09-22 14:47:50 +1200

    created route object and updated database call to store routes

--- database.php
+++ database.php
@@ -1,5 +1,6 @@
 <?php
 require_once("config.php");
+require_once("modles/Route.php");
 
 $conn = new mysqli($hostname, $username, $password, $database);
 
@@ -10,7 +11,7 @@ if ($conn->connect_error)
 }
 
 
-function getAllItems($conn)
+function getAllRoutes($conn)
 {
     $results = array();
     $query = "SELECT route_short_name, trips.route_id, trips.trip_id 
@@ -24,7 +25,17 @@ function getAllItems($conn)
     else {
         while ($row = $result->fetch_array(MYSQLI_ASSOC))
         {
-            $results[] = new Item($row['name'], $row['price'], $row['quantity']);
+            $current_route_id = $row['route_id'];
+            $route = new Route($row['route_short_name'], $row['route_id']);
+            $trip_ids = array();
+            while($row['route_id'] == $current_route_id){
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                if($row['route_id'] == $current_route_id){
+                    $trip_ids[] = $row['trip_id'];
+                }
+            }
+            $route.add_trips($trip_ids);
+            $results[] = $route;
         }
 
         $result->close();
