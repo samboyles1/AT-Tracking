64cac8e30dd7c7c6188cbc57e967bad8272ee36c
ZacKnopp <zac@netrix.co.nz>
2017-09-28 20:13:10 +1300

    BIG CHANGES
    Populate drop down (Only active buses)
    More than one bus moving

--- index.php
+++ index.php
@@ -8,36 +8,14 @@ $routes = getAllRoutes($conn);
 ?>
 
 <script>
-    function showTripsForRoute(route_id) {
-        console.log( route_id + " " +  
-            <?php echo $routes ?> 
-            );
-    }
+    var dbRoutes = <?php echo json_encode(getAllRoutes($conn)); ?>;
 </script>
 
 <div id="wrapper">
-<<<<<<< HEAD
+
     <div id="listDiv">
-        <select id="routeSelecter" style="width: 100px" onchange="showTripsForRoute(this.value)">
-            <?php
-            foreach($routes as $route) { ?>
-            <option value="<?php echo $route->get_id() ?>"><?php echo $route->get_name() ?></option>
-            <?php }?>
-        </select>
-=======
-    <div id="listWrapper">
-        <div><h1>Select a route</h1></div>
-        <div id="listDiv">
-            <select multiple size="28" id="routeSelecter">
-                <?php
-                $routes = getAllRoutes($conn);
-                foreach($routes as $route) { ?>
-                    <option id="options" value="<?php echo $route->get_id() ?>"><?php echo $route->get_name() ?></option>
-                <?php }?>
-            </select>
-        </div>
->>>>>>> origin/master
-    </div>
+        <select id="routeSelector" style="width: 100px" onchange="setSelectedRoute(this.value)"></select>
+
     <div id="mapdiv"></div>
 </div>
 
@@ -45,12 +23,8 @@ $routes = getAllRoutes($conn);
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBh1cr8Qd4TKgA4DVOhH5NBLNvgEgmqBg4&callback=setup">
 </script>
 <script src="scripts/map.js"></script>
-<script>
-    $(document).ready(function() {
-    });
 
-    var  routes_as_javascript = <?php echo json_encode(getAllRoutes($conn)); ?>;
-</script>
+
 <?php
 require_once 'include/footer.php';
 ?>
--- scripts/map.js
+++ scripts/map.js
@@ -1,41 +1,81 @@
 ﻿//These two globals are used in all the functions
+
 var googlemap = null;
-var busMarker = null;
 
-/**
- * called automatically when the googlemaps api is loaded
- */
+var selected_route_id = null;
+
+var markers = [];
+
+var api_url = 'https://api.at.govt.nz/v2/';
+var api_header = { 'Ocp-apim-Subscription-key': '093a87b71c14401f9ae9b72c9ace16a9' };
+
+
+// called automatically when the googlemaps api is loaded
+
 function setup() {
-    //make the map just once
-    initMap();
-    //get the intial bus position 
-    updateBusPosition();
-    //refresh bus position every 30 secs(is in milliseconds)
-    //setInterval(updateBusPosition, 30000);
-}
-
-
-/**
- * initialises the map
- */
-function initMap() {
     //this makes the map, centred in auckland(This point will get thrown away quickly when the bus moves)
     googlemap = new google.maps.Map(document.getElementById('mapdiv'), {
         zoom: 10,
         center: { lat: -36.848461, lng: 174.763336 }
     });
-
 }
 
 /**
- * calls the vehicle location api and returns a lat long for a single bus
- * moves marker to location of new lat lon of bus
+When the document is first loaded, get all the current routes and add them as options in the route select list
+*/
+$(document).ready(function () {
+    loadCurrentRoutes();
+});
+
+
+
+/**
+ * As soon as the document has loaded ask the api for ALL possible current routes
  */
-function updateBusPosition() {
+function loadCurrentRoutes() {
     $.ajax({
         type: "GET",
-        headers: { 'Ocp-Apim-Subscription-Key': '093a87b71c14401f9ae9b72c9ace16a9' },
-        url: 'https://api.at.govt.nz/v2/public/realtime',
+        headers: api_header,
+        url: api_url + 'public/realtime',
+        dataType: 'json',
+        success: function (data) {
+            var entities = data.response.entity;
+            entities.forEach(function (e) {
+                if (e.vehicle && e.vehicle.trip) {
+                    var routeName = getRouteName(e.vehicle.trip.route_id);
+                    $('#routeSelector').append(new Option(routeName, e.vehicle.trip.route_id));
+                }
+            });
+        }
+    });
+}
+
+function getRouteName(route_id) {
+    var route = dbRoutes.find(function (r) {
+        return r.id == route_id;
+    });
+    return route ? route.route_name : (route_id + ' not found');
+}
+
+function setSelectedRoute(value) {
+    selected_route_id = value;
+    updateBusPositions();
+    setInterval(updateBusPositions, 30000);
+}
+
+/**
+* calls the vehicle location api and returns a lat long for a single bus
+* moves marker to location of new lat lon of bus
+*/
+function updateBusPositions() {
+    if (selected_route_id == null) {
+        return;
+    }
+
+    $.ajax({
+        type: "GET",
+        headers: api_header,
+        url: api_url + 'public/realtime',
         dataType: 'json',
         success: function (data) {
             var vehicles = [];
@@ -45,27 +85,39 @@ function updateBusPosition() {
                     vehicles.push({ 'vehicle_id': e.vehicle.vehicle.id, 'latitude': e.vehicle.position.latitude, 'longitude': e.vehicle.position.longitude });
                 }
             });
-
-            var infoWindow = new google.maps.InfoWindow({
-                content: busInfoString
-            });
-
-            //The api returns the vehicles for ALL the active routes. 
-            //The forEach loop then just selects the route you asked for (selected_route_id)
-
-            //move the marker to the new bus lat long
-            busMarker.setPosition(busLatLng);
-            busMarker.setMap(googlemap);
-            busMarker.addListener('click', function() {
-               infoWindow.open(googlemap, busMarker)
-            });
-            //adjust the map so that the marker is in the middle of the map (will be zoomed in a lot with just one marker)
-            //the map will zoom out to fit more markers if they were added to the bounds
-            //this could be useful for multiple markers (https://stackoverflow.com/questions/1556921/google-map-api-v3-set-bounds-and-center?rq=1)
-            var bounds = new google.maps.LatLngBounds();
-            bounds.extend(busMarker.position);
-            googlemap.fitBounds(bounds);
+            if (vehicles.length > 0) {
+                showVehicles(vehicles);
+            }
+            else {
+                alert('No active buses on this route');
+            }
         }
-
     });
 }
+
+
+function showVehicles(vehicles) {
+    var bounds = new google.maps.LatLngBounds();
+
+    //gets rid of all existing markers
+    //when bus moves, old markers deleted, new markers made in new position
+    markers.forEach(function (m) {
+        m.setMap(null);
+    });
+
+    vehicles.forEach(function (v) {
+        var busMarker = new google.maps.Marker({
+            position: { lat: v.latitude, lng: v.longitude },
+            title: 'Bus: ' + v.vehicle_id,
+            map: googlemap
+        });
+
+        markers.push(busMarker);
+        //the map will zoom out to fit more markers if they were added to the bounds
+        bounds.extend(busMarker.position);
+    });
+
+    googlemap.fitBounds(bounds);
+
+}
+
