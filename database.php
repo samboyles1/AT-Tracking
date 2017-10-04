0af4ae9212966c89b1c40a7a0c1c847270d63a9b
ZacKnopp <zac@netrix.co.nz>
2017-10-05 10:33:11 +1300

    Code tidy
    Turns out we didn't need a join in the SQL query

--- database.php
+++ database.php
@@ -12,11 +12,11 @@ if ($conn->connect_error)
 
 function getAllRoutes($conn)
 {
-    $results = array();
-    $query = "SELECT routes.route_short_name, routes.route_long_name, trips.route_id, trips.trip_id 
-              FROM akl_transport.trips, akl_transport.routes
-              WHERE trips.route_id = routes.route_id;";
-    $dbresult = $conn->query($query);
+    $results = array();   
+    $other_query = "SELECT routes.route_id, routes.route_short_name, routes.route_long_name
+                    FROM akl_transport.routes";
+
+    $dbresult = $conn->query($other_query);
     if (!$dbresult)
     {
         fatalError($conn->error);
@@ -24,17 +24,7 @@ function getAllRoutes($conn)
     else {
         while ($row = $dbresult->fetch_array(MYSQLI_ASSOC))
         {
-
-            $current_route_id = $row['route_id'];
             $route = new Route($row['route_short_name'] . ' ' . $row['route_long_name'], $row['route_id']);
-            $trip_ids = array();
-            while($row['route_id'] == $current_route_id){
-                $row = $dbresult->fetch_array(MYSQLI_ASSOC);
-                if($row['route_id'] == $current_route_id){
-                    $trip_ids[] = $row['trip_id'];
-                }
-            }
-            $route->add_trips($trip_ids);
             $results[] = $route;
         }
         $dbresult->close();
